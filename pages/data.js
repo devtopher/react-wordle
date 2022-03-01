52ff677f8ca984d77e1ef4ac8fc1ea85e1a1d13a
devtopher <christopher.david.mccoy@gmail.com>
2022-02-28 22:31:54 -0500

    added props.submit to useeffect to make data work

--- pages/data.js
+++ pages/data.js
@@ -1,13 +1,16 @@
 import React, {useState} from "react"
 
-
-
 export default function useTophersData() {
     const [chars, setChars] = useState([])
-   
+    const [guess, setGuess] = useState([])
 
     return {
-        submit: function submit(){console.log("submitted")},
+        submit: function submit(){
+            console.log('AAA', `Why don't I have ${chars}`)
+            setGuess(chars)
+            
+            
+        },
         deleteChar: function deleteChar(){
             setChars(currCharacters => {
                 const newArray = [...currCharacters]
@@ -16,7 +19,6 @@ export default function useTophersData() {
             }) 
         },
         characterEntered: (newChar) => {
-            // if (currCharacters === 5){return null}
             setChars(currCharacters => {
                 if (currCharacters.length >= 5){return currCharacters}
                 return [...currCharacters, newChar]
@@ -26,8 +28,21 @@ export default function useTophersData() {
     }
 }
 
-const userStory1 = "As a user I can type and see it on the screen.✅"
-const userStory2 = "I can use backspace to remove typed characters✅"
-const userStory3 = "I can only type 5 letters at a time✅"
-const userStory4 = "I can submit my 5 letter word"
+// const userStory1 = "As a user I can type and see it on the screen.✅"
+// const userStory2 = "I can use backspace to remove typed characters✅"
+// const userStory3 = "I can only type 5 letters at a time✅"
+// const userStory4 = "I can submit my 5 letter word✅"
+// const userStory5 = "When I submit, my word is locked in place"
+// const userStory5.5 = "When I submit, my word is evaluated to be in the list or not"
+// const userStory6 = "When I submit, my word is compared to the word"
+// const userStory7 = "When my word is compared, letters that are in the word and in the correct place change to green"
+// const userStory8 = "When my word is compared, letters that are in the word and not in the correct place change to yellow"
+// const userStory8 = "When my word is compared, letters that are not in the word change to grey"
+// const userStory10 = "When my word is compared, if it is correct, I win and get a module"
+// const userStory9 = "When the above happens, the letters also change on the visual keyboard"
+// const userStory10 = "Once my word has been evaluated and the colors are updated, I can type on a new line a new guess"
+// const userStory10 = "If I submit 6 times, I lose"
+// const userStory10 = "When I submit, my word is locked in place"
+// const userStory10 = "When I submit, my word is locked in place"
+// const userStory10 = "When I submit, my word is locked in place"
 
